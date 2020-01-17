b4e247a3b4543e11bc65160a430e4a7d51f47c1f
aoguz <oguzabdullah39@gmail.com>
2020-01-18 00:11:50 +0300

    form verilerinin kontrolü ve kaydedilemesi ile ilgili örnek yapildi

--- lib/textform_fieldOrnek.dart
+++ lib/textform_fieldOrnek.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class TextFormFieldOrnek extends StatefulWidget {
@@ -10,19 +8,21 @@ class TextFormFieldOrnek extends StatefulWidget {
 }
 
 class _TextFormFieldOrnekState extends State<TextFormFieldOrnek> {
+  String _adSoyad, _email, _sifre;
+  final formKey=GlobalKey<FormState>(); // formkey tanımlıyoruz formState verilerinin içinden alavailceği 
+  bool otoValidate =false;
   @override
   Widget build(BuildContext context) {
     return Theme(
-
-      data: ThemeData( // genel olarak tanımlandığı sayfa üzerinde tema değişiklikleri yapmamızı sağlar. Buraya tanımlanan tema stilleri sayfa içerisindeki widgetlere uygulanır. Eğer tasarımımıza uygun stilleri buraya yazarsak widgetlerin içlerinde renk düzenlemesi yapmamıza gerek kalmaz gerekli stil özellikleri themeData üzeirnden alır.
-
-          primaryColor: Colors.deepPurple, //genel olarak baskın renk olarak kabul görür ve primaryColor'a ne verirsek o bizim karşımıza değer olarak çıkar. 
-          accentColor: Colors.lime,  // floatingActionButton rengini değiştirir.
-
-          
-
+      data: ThemeData(
+        errorColor: Colors.red,
+        // genel olarak tanımlandığı sayfa üzerinde tema değişiklikleri yapmamızı sağlar. Buraya tanımlanan tema stilleri sayfa içerisindeki widgetlere uygulanır. Eğer tasarımımıza uygun stilleri buraya yazarsak widgetlerin içlerinde renk düzenlemesi yapmamıza gerek kalmaz gerekli stil özellikleri themeData üzeirnden alır.
+        hintColor: Colors
+            .black38, // inputların içinde yazı olarak var olan hint özelliğinin rengini düzenleriz.
+        primaryColor: Colors
+            .deepPurple, //genel olarak baskın renk olarak kabul görür ve primaryColor'a ne verirsek o bizim karşımıza değer olarak çıkar.
+        accentColor: Colors.lime, // floatingActionButton rengini değiştirir.
       ),
-         
       child: Scaffold(
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
@@ -33,44 +33,112 @@ class _TextFormFieldOrnekState extends State<TextFormFieldOrnek> {
           ),
           body: Padding(
             padding: EdgeInsets.all(18),
-            child: ListView(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      // hintText: "Ad Soyad",
-                      labelText: "Ad Soyad",
-                      prefixIcon: Icon(Icons.account_circle),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      )),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      // hintText: "Ad Soyad",
-                      labelText: "E-mail adresinizi giriniz",
-                      prefixIcon: Icon(Icons.account_circle),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(),
-                      )),
-                ),
-                RaisedButton.icon(
-                  icon: Icon(
-                    Icons.save,
-                    color: Colors.white,
+            child: Form(
+              key: formKey, // 
+              autovalidate: otoValidate, //forma girilen değerleri otomatik validate işlemini yapar, devamlı doğruluğunu denetler
+              child: ListView(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelStyle: TextStyle(),
+                        // hintText: "Ad Soyad",
+                        labelText: "Ad Soyad",
+                        prefixIcon: Icon(Icons.account_circle),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        )),
+                        validator: _isimKontrol,
+                          onSaved: (isimVeri)=> _adSoyad =isimVeri,
                   ),
-                  label: Text(
-                    "Kaydet",
-                    style: TextStyle(color: Colors.white),
+                  SizedBox(
+                    height: 10,
                   ),
-                  onPressed: () {},
-                  color: Colors.green,
-                )
-              ],
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        // hintText: "Ad Soyad",
+                        labelText: "E-mail adresinizi giriniz",
+                        prefixIcon: Icon(Icons.account_circle),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        )),
+                        validator: _emailKontrol,
+                        onSaved: (emailVeri)=> _email =emailVeri,
+
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    obscureText:
+                        true, // şifre girilirken karakterleri " *  " işareti ile kapatır.
+                    decoration: InputDecoration(
+                        // hintText: "Ad Soyad",
+                        labelText: "Şifrenizi giriniz",
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(),
+                        )),
+                        validator: (String girilenVeri){
+                            if (girilenVeri.length <6) {
+                              return "En az 6 karakter yeterli";
+                            }
+
+                        },
+                          onSaved: (sifreVeri)=> _sifre =sifreVeri,
+                  ),
+                  RaisedButton.icon(
+                    icon: Icon(
+                      Icons.save,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      "Kaydet",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        formKey.currentState.save();
+                        debugPrint("Email $_email , Ad Soyad : $_adSoyad , Şifre : $_sifre");
+                      }
+                      else{
+
+                        setState(() {
+                          otoValidate =true;
+                        });
+                        
+                      }
+
+                    },
+                    color: Colors.green,
+                  )
+                ],
+              ),
             ),
-          )), 
+          )),
     );
+  }
+  String _emailKontrol (String email){
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+      if (!regex.hasMatch(email)) {
+        return "Geçersiz e-mail";
+      }
+      else {
+        return null;
+      }
+
+  }
+
+
+  String _isimKontrol(String isim) {
+      RegExp regex=RegExp("^[a-zA-Z]+\$");
+
+      if (!regex.hasMatch(isim)) { 
+        return "İsim Numara İçermemeli";
+      }
+      else return null;
+
+
   }
 }
